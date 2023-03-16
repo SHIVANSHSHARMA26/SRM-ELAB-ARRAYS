a911cab7fc960beed07d90b35b5595f7e8e418c4
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2023-03-16 23:54:30 +0530

    Create Simon is studying.cpp

---
+++ Simon is studying.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++)
+{
+cin>>arr[i];
+}
+  int i;
+unordered_map<int, int> hash;
+for(i= 0;i< n;i++)
+hash[arr[i]]++;
+int max_count = 0, res = -1;
+for (auto i : hash) {
+if (max_count < i.second) {
+res = i.first;
+max_count = i.second;
+}
+}
+cout<<res<<"\n";
+return 0;
+}
